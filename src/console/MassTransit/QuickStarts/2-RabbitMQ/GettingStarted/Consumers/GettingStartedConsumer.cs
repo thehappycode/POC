8561c0df1597ff3a216edbfac0491ed365a91deb
thehappycode <thehappycode@hotmail.com>
2023-10-24 14:21:30 +0700

    #mass-transit - first commit

---
+++ src/console/MassTransit/QuickStarts/2-RabbitMQ/GettingStarted/Consumers/GettingStartedConsumer.cs
@@ -0,0 +1,24 @@
+namespace GettingStarted.Consumers
+{
+    using System.Threading.Tasks;
+    using MassTransit;
+    using Contracts;
+    using Microsoft.Extensions.Logging;
+
+    public class GettingStartedConsumer :
+        IConsumer<GettingStarted>
+    {
+        private readonly ILogger<GettingStartedConsumer> _logger;
+
+        public GettingStartedConsumer(ILogger<GettingStartedConsumer> logger)
+        {
+            _logger = logger;
+        }
+        public Task Consume(ConsumeContext<GettingStarted> context)
+        {
+            _logger.LogInformation("Received Text: {Text}", context.Message.Value);
+            
+            return Task.CompletedTask;
+        }
+    }
+}
