29f579b37d262f64dbf40977660fa189d8ea892c
thehappycode <thehappycode@hotmail.com>
2023-10-29 11:30:47 +0700

    #serilog - StructuredData

---
+++ src/console/Serilog/StructuredData/MyClass.cs
@@ -0,0 +1,5 @@
+namespace StructuredData;
+
+public class MyClass {
+    public int MyProperty { get; set; }
+}
---
+++ src/console/Serilog/StructuredData/Program.cs
@@ -0,0 +1,36 @@
+﻿using Serilog;
+using StructuredData;
+
+SourceContexts();
+
+Correlation();
+
+static void SourceContexts()
+{
+    Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console() // Writes log events into console
+            .WriteTo.File("./log-source-contexts-.txt", rollingInterval: RollingInterval.Day) // Writes log event into file text
+            .CreateLogger();
+    var myLog = Log.ForContext<MyClass>();
+    myLog.Information("Hello world!");
+
+    // Finally, once just before the application exits...
+    Log.CloseAndFlush();
+
+}
+
+
+static void Correlation()
+{
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console() // Writes log events into console
+        .WriteTo.File("./log-source-contexts-.txt", rollingInterval: RollingInterval.Day) // Writes log event into file text
+        .CreateLogger();
+
+    Log.Information("Hello world!");
+
+    var log = Log.ForContext("JobId", Guid.NewGuid());
+    log.Information("Running a new job");
+    // Finally, once just before the application exits...
+    Log.CloseAndFlush();
+}
